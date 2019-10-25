20e118b71dd98b54e7e6447d6e6e797e0cbc94f9
杜骁 <duxiao@sweetysoft.com>
2019-10-25 23:22:13 +0800

    将rack-cors抽取到initializers文件夹中

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,5 @@ module GrapeApi
     config.load_defaults 5.2
     config.api_only = true
     config.debug_exception_response_format = :api
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
   end
 end
