85759edb236790e09bc5e4762341c3a494df85fa
杜骁 <duxiao@sweetysoft.com>
2019-10-28 14:47:59 +0800

    调整了接口

--- app/api/exchange/home/welcome.rb
+++ app/api/exchange/home/welcome.rb
@@ -1,16 +1,16 @@
 class Exchange::Home::Welcome < Grape::API
-  resource :welcome do
-    desc 'lalala' do
+  namespace :home do
+    resource :welcome do
+      desc '欢迎页面'
+      params do
+        requires :name, type: String
+        optional :time, type: Integer
+      end
+      get :hello do
+        {
+          message: "hi === #{params[:name]} === #{params[:time]}"
+        }
+      end
     end
-    params do
-      requires :name, type: String
-      optional :time, type: Integer
-    end
-    get :hello do
-      {
-        message: "hi === #{params[:name]} === #{params[:time]}"
-      }
-    end
-
   end
 end
--- app/api/exchange/root_api.rb
+++ app/api/exchange/root_api.rb
@@ -2,10 +2,15 @@ require 'grape'
 require 'grape-swagger'
 class Exchange::RootAPI < Grape::API
   include APIDefault
-  desc 'API Root'
+
   version 'v2', using: :path
   mount Home::Welcome
+
   add_swagger_documentation(
-    schemes: ['http']
+    version: "1.0.0",
+    info: {
+      title: 'CADAE MOBILE API',
+    },
+    schemes: ['http'],
   )
 end
